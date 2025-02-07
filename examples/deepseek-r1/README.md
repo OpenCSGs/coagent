558ce8a9f2f57b56c8891b8901473f65659c02ef
RussellLuo <luopeng.he@gmail.com>
2025-02-07 09:11:53 +0800

    Add an example of using DeepSeek-R1

---
+++ examples/deepseek-r1/README.md
@@ -0,0 +1,22 @@
+# DeepSeek-R1
+
+This example demonstrates how to implement an agent that uses [DeepSeek-R1][1], the [reasoning model][2] released by DeepSeek.
+
+
+## Prerequisites
+
+- Install `coagent` (see [Installation](../../README.md#installation)).
+
+
+## Quick Start
+
+Run the agent:
+
+```bash
+export DEEPSEEK_API_KEY="your-api-key"
+python examples/deepseek-r1/agent.py
+```
+
+
+[1]: https://api-docs.deepseek.com/news/news250120
+[2]: https://api-docs.deepseek.com/guides/reasoning_model
---
+++ examples/deepseek-r1/agent.py
@@ -0,0 +1,48 @@
+import asyncio
+import os
+
+from coagent.agents import ChatAgent, ChatMessage, ModelClient
+from coagent.core import AgentSpec, new, set_stderr_logger
+from coagent.runtimes import LocalRuntime
+
+
+client = ModelClient(
+    model="openai/deepseek-reasoner",
+    api_base="https://api.deepseek.com/v1",
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+)
+
+
+deepseek_reasoner = AgentSpec("deepseek_reasoner", new(ChatAgent, client=client))
+
+
+async def main():
+    async with LocalRuntime() as runtime:
+        await runtime.register(deepseek_reasoner)
+
+        result = await deepseek_reasoner.run(
+            ChatMessage(
+                role="user", content="9.11 and 9.8, which is greater?"
+            ).encode(),
+            stream=True,
+        )
+
+        reasoning_started = False
+        reasoning_stopped = False
+        async for chunk in result:
+            msg = ChatMessage.decode(chunk)
+            if msg.reasoning_content:
+                if not reasoning_started:
+                    print("<think>", flush=True)
+                    reasoning_started = True
+                print(msg.reasoning_content, end="", flush=True)
+            if msg.content:
+                if not reasoning_stopped:
+                    print("</think>", flush=True)
+                    reasoning_stopped = True
+                print(msg.content, end="", flush=True)
+
+
+if __name__ == "__main__":
+    set_stderr_logger()
+    asyncio.run(main())
